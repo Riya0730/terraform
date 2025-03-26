4ac3cec0e9f5326c2effccb1329ddcab0fb18183
Riya Nemade <riyanemade7@gmail.com>
2025-03-26 19:07:32 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "my_region" {
+  type    = string
+  default = "ap-south-1"
+}
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
+variable "ami" {
+  type    = string
+  default = "ami-05c179eced2eb9b5b"
+}
+variable "instance_type" {
+  type = string
+}
